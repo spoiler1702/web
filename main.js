4cef8497cbf6bbcd21e43ad0810492bbf59e9743
spoiler1702 <143501051+spoiler1702@users.noreply.github.com>
2024-02-15 21:11:08 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Điền thông tin cá nhân của bạn vào các thẻ HTML
+    document.getElementById("name").innerText = "John Doe";
+    document.getElementById("occupation").innerText = "Developer";
+    document.getElementById("interests").innerText = "Coding, reading, hiking";
+    document.getElementById("email").innerText = "john.doe@example.com";
+  });
+  
